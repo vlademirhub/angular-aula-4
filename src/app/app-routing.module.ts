1b091002bae1e47a0a091900ab0ae710673ebf2b
vlademirhub <vlademir98@hotmail.com>
2018-04-27 21:03:38 -0300

    Primeiro Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { Pagina1Component } from './pagina1/pagina1.component';
-//import { Pagina2Component } from './pagina2/pagina2.component';
+import { HomeComponent } from './home/home.component';
+import { ServicoComponent } from './servico/servico.component';
+import { ContatoComponent } from './contato/contato.component';
 
 const routes: Routes = [
-//{ path: '', redirectTo: '/pagina1', pathMatch: 'full' },
-//{ path: 'pagina1', component: Pagina1Component },
-//{ path: 'pagina2', component: Pagina2Component }
-
+{ path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: 'home', component: HomeComponent },
+{ path: 'servico', component: ServicoComponent },
+{ path: 'contato', component: ContatoComponent }
 ];
 
 @NgModule({
+
 imports: [ RouterModule.forRoot(routes) ],
+
 exports: [ RouterModule ]
+
 })
 
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ServicoComponent } from './servico/servico.component';
+import { ContatoComponent } from './contato/contato.component';
+
+import { HeadComponent } from './head/head.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicoComponent,
+    ContatoComponent,
+
+    HeadComponent
   ],
   imports: [
     BrowserModule,
